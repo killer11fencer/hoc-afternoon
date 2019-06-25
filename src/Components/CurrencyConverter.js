a005ec6225c8faed9b4396aac9447308ec19aa00
Jeoffrey <jeoff.howe@gmail.com>
2019-06-24 23:44:08 -0600

    step 4

--- src/Components/CurrencyConverter.js
+++ src/Components/CurrencyConverter.js
@@ -2,9 +2,32 @@ import React from 'react'
 
 const withCurrency = (BaseComponent) => {
     class Currency extends Component {
+        state = {
+            currencyChosen: false,
+            selectedCurrency: 'Select Currency',
+            amount: 0
+        }
         render() {
+            const currencyData = [
+                { name: 'Japanese Yen', symbol: '¥', rate: 113.6, id: 0 },
+                { name: 'British Pound', symbol: '£', rate: 0.77, id: 1 },
+                { name: 'Canadian Dollar', symbol: 'CAD', rate: 1.32, id: 2 },
+                { name: 'Mexican Peso', symbol: 'Mex$', rate: 20.41, id: 3 },
+                { name: 'Swiss Franc', symbol: 'Fr.', rate: 1.01, id: 4 }
+            ]
+            const currencyOptions = currencyData.map((currency,i)=>{
+                return <option value={i} key={i}>{currency.name}</option>
+            })
             return(
-                <div></div>
+                <div>
+                    <select value={this.state.selectedCurrency}>
+                    <option value='Select Currency'>Select Currency</option>
+                    {currencyOptions}
+                    </select>
+                    <BaseComponent currency={this.state.selectedCurrency} amount={this.state.amount}/>
+                    <button>+</button>
+                    <button>-</button>
+                </div>
             )
         }
         }
