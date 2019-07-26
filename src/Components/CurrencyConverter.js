756045ef6ae752bd00e0aa41220f600d4d64f66e
Jeoffrey <jeoff.howe@gmail.com>
2019-07-25 23:01:34 -0600

    hoc practice

--- src/Components/CurrencyConverter.js
+++ src/Components/CurrencyConverter.js
@@ -1,50 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const withCurrency = (BaseComponent) => {
+const withCurrency = (BaseComponent) => (
     class Currency extends Component {
         state = {
             currencyChosen: false,
             selectedCurrency: 'Select Currency',
             amount: 0
         }
-        handleAmountIncrease = () => {
-            this.setState((prevState)=> {
-                return {amount: (prevState.amount += 1)}
-            })
-        }
-        handleAmountDecrease = () => {
-            this.setState((prevState)=> {
-                return { amount: (prevState.amount -= 1)}
-            })
-        }
-        handleOptionSelect = (e) => {
-            this.setState(()=> {
-                return {selectedCurrency: e.target.value,
-                        currencyChosen: e.target.value === 'Select Currency' ? false : true}
-            })
-        }
         render() {
             const currencyData = [
-                { name: 'Japanese Yen', symbol: '¥', rate: 113.6, id: 0 },
-                { name: 'British Pound', symbol: '£', rate: 0.77, id: 1 },
-                { name: 'Canadian Dollar', symbol: 'CAD', rate: 1.32, id: 2 },
-                { name: 'Mexican Peso', symbol: 'Mex$', rate: 20.41, id: 3 },
-                { name: 'Swiss Franc', symbol: 'Fr.', rate: 1.01, id: 4 }
+                { name: 'Japanese Yen', symbol: '¥', rate: 113.6 },
+                { name: 'British Pound', symbol: '£', rate: 0.77 },
+                { name: 'Canadian Dollar', symbol: 'CAD', rate: 1.32 },
+                { name: 'Mexican Peso', symbol: 'Mex$', rate: 20.41 },
+                { name: 'Swiss Franc', symbol: 'Fr.', rate: 1.01 }
             ]
-            const currencyOptions = currencyData.map((currency,i)=>{
-                return <option value={i} key={i}>{currency.name}</option>
+            let currencyOptions = currencyData.map((currency,id)=>{
+                return <option value={currency.rate} key={id}>{currency.name}</option>
             })
-            return(
-                <div>
-                    <select value={this.state.selectedCurrency}>
-                    <option value='Select Currency'>Select Currency</option>
-                    {currencyOptions}
-                    </select>
-                    <BaseComponent currency={this.state.selectedCurrency} amount={this.state.amount}/>
-                    <button>+</button>
-                    <button>-</button>
-                </div>
-            )
-        }
+            return ()
         }
     }
+)
